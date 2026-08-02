4bd9fb2b8e6a3bf7a99f31bc4a312c6a9407b98a
Lprabodha <lprabodha1998@gmail.com>
2026-08-02 21:21:54 +0530

    Add main pages and functionality for online ticket booking system

--- booking/views.py
+++ booking/views.py
@@ -3,4 +3,28 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, "booking/home.html")
+    
+    # fruits = ["Apple", "Orange", "Mango"]
+    fruits = []
+    
+    return render(request, "booking/home.html", {
+        'fruits' : fruits
+    })
+
+def contact(request):
+    
+    context = {
+        "user_data" :{
+            "name": "Nimal",
+            "email": "nimal@gmail.com"
+        }
+    }
+    
+    return render(request, "public/contact.html", context)
+
+def about(request):
+    return render(request, "public/about-us.html")
+
+def faq(request):
+    return render(request, "public/faq.html")
+    
