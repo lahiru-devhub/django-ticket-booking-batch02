3fab55c521446450b3549fcbda45639e0b6c7357
Lprabodha <lprabodha1998@gmail.com>
2026-08-02 20:37:03 +0530

    Implement base template and initial page structure with Tailwind CSS integration

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'booking'
+    'booking',
+    "tailwind",
+    "theme",
+    
+]
+
+if DEBUG:
+    INSTALLED_APPS += [
+    "django_browser_reload",
 ]
 
 MIDDLEWARE = [
@@ -122,3 +130,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+TAILWIND_APP_NAME = "theme"
